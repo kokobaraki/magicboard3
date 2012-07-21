e40c9414944431412c0034b8418ddf4af766e1b7
kevin <kevinpark@dppia.com>
2012-07-21 13:32:21 +0900

    dbconfig 파일 삭제

--- magic/skin/Write/blog/controller/can.file_upload.php
+++ magic/skin/Write/blog/controller/can.file_upload.php
@@ -1,9 +1,1 @@
-<?php if(!defined("__MAGIC__")) exit; 
-
-$board = Board::Inst($this->bo_id);
-
-$pass=false;
-if(
-	$board->bo_use_file &&
-	(Member::Level() >= intVal($board->bo_file_level_upload))
-) $pass = true;
+../../basic/controller/can.file_upload.php
--- magic/skin/Write/blog/controller/can.list.php
+++ magic/skin/Write/blog/controller/can.list.php
@@ -1,7 +1,1 @@
-<?php if(!defined("__MAGIC__")) exit; 
-
-$pass=false;
-if(
-	$this->Config('mb','admin') ||
-	$this->Config('mb','level') >= intval(Board::Inst($this->bo_id)->bo_level_list)
-) $pass=true;
+../../basic/controller/can.list.php
